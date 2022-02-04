b2ad25fb9ec10bda65c4452ec103ef3cd24e96cd
Yatri Trivedi <jivanyatra@gmail.com>
2022-02-04 05:05:33 +0000

    changed boilerplate to work with env vars

--- zalgo_slackbot.py
+++ zalgo_slackbot.py
@@ -3,8 +3,9 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Install the Slack app and get xoxb- token in advance
-app = App(token="SLACK_BOT_TOKEN")
+app = App(token=SLACK_BOT_TOKEN,
+          signing_secret=SIGNING_SECRET)
 
 if __name__ == "__main__":
     load_dotenv()
-    SocketModeHandler(app, "SLACK_APP_TOKEN").start()
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
