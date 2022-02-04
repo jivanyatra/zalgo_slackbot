2feaf099574ed35ac33a3ce7d1b8440d439c4ec0
Yatri Trivedi <jivanyatra@gmail.com>
2022-02-04 05:02:47 +0000

    added slack_bolt and .env boilterplate

---
+++ zalgo_slackbot.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token="SLACK_BOT_TOKEN")
+
+if __name__ == "__main__":
+    load_dotenv()
+    SocketModeHandler(app, "SLACK_APP_TOKEN").start()
