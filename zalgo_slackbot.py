0b861059cb57d48a17d5cf1b32ba4a4ce13fc175
Yatri Trivedi <jivanyatra@gmail.com>
2022-02-04 05:23:51 +0000

    added basic slash commands - NOT tested yet

--- zalgo_slackbot.py
+++ zalgo_slackbot.py
@@ -1,10 +1,23 @@
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from zalgolibpy import enzalgofy, dezalgofy
 
 # Install the Slack app and get xoxb- token in advance
 app = App(token=SLACK_BOT_TOKEN,
           signing_secret=SIGNING_SECRET)
+
+@app.command("/enzalgofy")
+def convert_to_zalgo(ack, respond, command):
+    # Acknowledge command request
+    ack()
+    respond(f"{enzalgofy(command['text'])}")
+
+@app.command("/dezalgofy")
+def convert_from_zalgo(ack, respond, command):
+    # Acknowledge command request
+    ack()
+    respond(f"{dezalgofy(command['text'])}")
 
 if __name__ == "__main__":
     load_dotenv()
